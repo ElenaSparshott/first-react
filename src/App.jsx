cdafe3cfe422659a89fb8669f262e5f35e4fb077
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2025-03-24 18:38:13 +0000

    components file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-function App() {
+import Header from "./components/header"
+import MainContent from "./components/mainContent"
+import Footer from "./components/footer"
+
+export default function App() {
 
       return (
         <>
@@ -7,43 +11,4 @@ function App() {
             <Footer />
         </>
     )
-
-function Header() {
-    return (
-        <header className="header">
-            <img src="/src/assets/react.svg" className="nav-logo" alt="React logo" />
-            <nav>
-                <ul className="nav-list">
-                    <li className="nav-list-item">Pricing</li>
-                    <li className="nav-list-item">About</li>
-                    <li className="nav-list-item">Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
 }
-
-function MainContent() {
-    return (
-        <main>
-            <h1>Reason I am excited to learn React</h1>
-            <ol>
-                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-                <li>I am more likely to get a job as a front end developer if I know React</li>
-            </ol>
-        </main>
-    )
-}
-
-function Footer() {
-    return (
-        <footer className="footer">
-            <small>© 2024 Ziroll development. All rights reserved.</small>
-        </footer>
-    )
-}
-
-
-}
-
-export default App
